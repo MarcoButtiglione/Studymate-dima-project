803559a6c04d605652d3cf9d5a8fd281db870d9f
Marco Buttiglione <marco.buttiglione98@gmail.com>
2023-05-19 15:15:11 +0200

    Removed some import

--- studymate/lib/screens/Authenticated/profilePage/own_profile_page.dart
+++ studymate/lib/screens/Authenticated/profilePage/own_profile_page.dart
@@ -1,19 +1,13 @@
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:provider/provider.dart';
-import 'package:studymate/provider/AuthService.dart';
 import 'package:studymate/provider/authentication.dart';
 import 'package:studymate/screens/Authenticated/profilePage/updateInterest.dart';
-import 'package:studymate/screens/Login/login.dart';
 import 'package:studymate/service/storage_service.dart';
 import 'package:path/path.dart' as p;
 
@@ -88,7 +82,6 @@ class _OwnProfilePageState extends State<OwnProfilePage> {
     return File(croppedImage.path);
   }
 
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final user = FirebaseAuth.instance.currentUser!;
   bool _isSigningOut = false;
 
